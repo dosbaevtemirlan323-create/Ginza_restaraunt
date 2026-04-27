a772d062ef23574d1a18ec9a6083dbf7b4c4ccd5
dosbaevtemirlan323-create <dosbaevtemirlan323@gmail.com>
2026-04-27 08:46:00 +0500

    Update asgi.py

--- restaurant/asgi.py
+++ restaurant/asgi.py
@@ -4,7 +4,8 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import main.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant.settings')
+application = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
