0024c2894b1444070da4630c19a2b5e3b94f5b24
dosbaevtemirlan323-create <dosbaevtemirlan323@gmail.com>
2026-06-23 12:30:57 +0500

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,6 +94,9 @@ DATABASES = {
     }
 }
 
+# Если Railway передал переменную с URL базы данных, используем её
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
