5003d0c492bb2c94c695d6d85064fb191402b4a5
dosbaevtemirlan323-create <dosbaevtemirlan323@gmail.com>
2026-06-23 13:24:15 +0500

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -135,8 +135,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Настройки почты для отправки чеков
 # Настройки почты для отправки чеков и сброса пароля через Gmail
 # Настройки почты: выводим всё в консоль сервера, обходя любые блокировки портов
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-EMAIL_FAIL_SILENTLY = True
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'apikey'  # всегда 'apikey'
+EMAIL_HOST_PASSWORD = 'sbdz fmvr zzjv lzep'
 # Sites framework
 SITE_ID = 1
 
